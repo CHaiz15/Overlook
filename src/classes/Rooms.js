ca72da586d08213252f9a23af260171f4b184af6
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-11 13:12:35 -0700

    Add class methods skeleton

--- src/classes/Rooms.js
+++ src/classes/Rooms.js
@@ -1,5 +1,31 @@
 class Rooms {
   constructor() {
+    this.rooms;
+    this.bookings;
+    this.todaysDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+  }
+  customersPastNights(){
+
+  }
+  availableRooms(){
+
+  }
+  futureNights(){
+
+  }
+  FilterByType(){
+
+  }
+  FilterByDate(){
+
+  }
+  percentOfRoomsAvailable(){
+
+  }
+  todaysAvailableRooms(){
+
+  }
+  calculateRooms(){
 
   }
 }
--- src/classes/User.js
+++ src/classes/User.js
@@ -1,6 +1,13 @@
 class User {
   constructor() {
-
+    this.futureBookings;
+    this.id;
+    this.name;
+    this.datePicked;
+    this.currentSpent;
+  }
+  bookRoom(){
+    
   }
 }
 
