768e87a1d6b7bf8ab1def1a95b2c1c7fe449e7f3
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-12 15:27:29 -0700

    Add function to display nights

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -10,3 +10,12 @@ export function openUserInterface(usersName) {
   $('body').css({background: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(../images/user-image.jpg) center / cover"});
   $('#title').text(`Welcome, ${usersName}!`);
 }
+
+export function instantiateFutureAndPastNights(pastNights, futureNights, userID) {
+  pastNights.forEach(night => {
+    $('.past-nights-list').append(`<li>You booked room #${night.roomNumber} on ${night.date}.</li>`)
+  })
+  futureNights.forEach(night => {
+    $('.future-nights-list').append(`<li>You have room #${night.roomNumber} booked for ${night.date}.</li>`)
+  })
+}
