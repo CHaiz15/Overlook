dec0a5347de43c49b71eefab62993fecb6556ead
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-12 15:20:09 -0700

    Add users bookings methods

--- src/classes/Rooms.js
+++ src/classes/Rooms.js
@@ -2,27 +2,42 @@ class Rooms {
   constructor(roomsData, bookingsData) {
     this.rooms = roomsData;
     this.bookings = bookingsData;
-    this.todaysDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    this.todaysDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
   }
-  customersPastNights(){
+  userBookings(usersId) {
+    return this.bookings.filter(booking => {
+      return booking.userID === usersId;
+    })
+  }
+  userPastNights(userBookings, userId) {
+    return userBookings.filter(booking => {
+      return booking.date < this.todaysDate;
+    })
+  }
+  userFutureNights(userBookings, userId) {
+    return userBookings.filter(booking => {
+      return booking.date > this.todaysDate;
+    })
+  }
+  bookCustomerRoom() {
 
   }
-  availableRooms(){
+  availableRooms() {
 
   }
-  FilterByType(){
+  FilterByType() {
 
   }
-  FilterByDate(){
+  FilterByDate() {
 
   }
-  percentOfRoomsAvailable(){
+  percentOfRoomsAvailable() {
 
   }
-  todaysAvailableRooms(){
+  todaysAvailableRooms() {
 
   }
-  calculateRooms(){
+  calculateRooms() {
 
   }
 }
