69b14a82627b304a7924bb6744edea81e165d7a8
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-12 15:28:47 -0700

    Update checkLoginInfo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/base.scss';
 import Rooms from '../src/classes/Rooms.js';
 import User from '../src/classes/User.js';
 import Manager from '../src/classes/Manager.js';
-import {loginError, openUserInterface} from '../src/domUpdates.js';
+import {loginError, openUserInterface, instantiateFutureAndPastNights} from '../src/domUpdates.js';
 
 // Classes
 let rooms;
@@ -31,16 +31,13 @@ Promise.all([roomsData, usersData, bookingsData])
 function checkLoginInfo(roomsData, usersData, bookingsData) {
   let userId = parseInt($('#username-input').val().slice(8));
   let passwordCheck = $('#password-input').val() === 'overlook2019';
-  let userIdCheck = usersData.find(foundUser => {
-    return foundUser.id === userId;
-  });
-  console.log(userIdCheck);
+  let userIdCheck = usersData.find(foundUser => foundUser.id === userId);
   if ($('#username-input').val() === 'manager' && passwordCheck) {
     let manager = new Manager();
   } else if ($('#username-input').val().slice(0, 8) === 'customer' && passwordCheck && userIdCheck) {
-    let user = new User(bookingsData, usersData, userId);
-    console.log(user.name);
+    let user = new User(rooms.userBookings(userId), usersData.find(user => user.id === userId).name, userId);
     openUserInterface(user.name);
+    instantiateFutureAndPastNights(rooms.userPastNights(user.userBookings, userId), rooms.userFutureNights(user.userBookings, userId));
   } else {
     loginError();
   }
